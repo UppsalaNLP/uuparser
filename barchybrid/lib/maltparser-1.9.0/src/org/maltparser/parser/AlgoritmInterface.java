a48af80f76cec3365facece57b3f321c89d0bea3
miryam <mdelhoneux@gmail.com>
2017-08-17 14:08:03 +0200

    we don't need MaltParser anymore

--- barchybrid/lib/maltparser-1.9.0/src/org/maltparser/parser/AlgoritmInterface.java
+++
@@ -1,8 +0,0 @@
-package org.maltparser.parser;
-
-
-public interface AlgoritmInterface {
-	public ParserRegistry getParserRegistry();
-	public ParserConfiguration getCurrentParserConfiguration();
-	public DependencyParserConfig getManager();
-}
