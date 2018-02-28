7d128b8a4a95464d3e14712b56bfeff784f8ca99
miryam <mdelhoneux@gmail.com>
2018-02-28 18:28:20 +0100

    easier to reprod error

--- barchybrid/src/multilayer_perceptron.py
+++ barchybrid/src/multilayer_perceptron.py
@@ -2,19 +2,18 @@ import dynet as dy
 
 class MLP(object):
     def __init__(self, model, in_dim, hid_dim, out_dim, activation=dy.tanh):
-        ##
-        model = dy.ParameterCollection()
         self._W1 = model.add_parameters((hid_dim, in_dim))
         self._b1 = model.add_parameters(hid_dim)
         self._W2 = model.add_parameters((out_dim, hid_dim))
         self._b2 = model.add_parameters(out_dim)
 
-        #WORKS
-        #self.model = dy.ParameterCollection() # this works
-        #self._W1 = self.model.add_parameters((hid_dim, in_dim))
-        #self._b1 = self.model.add_parameters(hid_dim)
-        #self._W2 = self.model.add_parameters((out_dim, hid_dim))
-        #self._b2 = self.model.add_parameters(out_dim)
+        """ uncomment this and it will work
+        self.model = dy.ParameterCollection() # this works
+        self._W1 = self.model.add_parameters((hid_dim, in_dim))
+        self._b1 = self.model.add_parameters(hid_dim)
+        self._W2 = self.model.add_parameters((out_dim, hid_dim))
+        self._b2 = self.model.add_parameters(out_dim)
+        """
 
         self.activation = activation
 
