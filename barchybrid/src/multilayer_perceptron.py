8dffa4cf7793e65921d66b9756487e7cef2bb344
miryam <mdelhoneux@gmail.com>
2018-02-28 18:24:51 +0100

    this is what does not work

--- barchybrid/src/multilayer_perceptron.py
+++ barchybrid/src/multilayer_perceptron.py
@@ -1,19 +1,19 @@
 import dynet as dy
 
 class MLP(object):
-    def __init__(self, model, in_dim, hid_dim, out_dim, activation=dy.tanh):
+    def __init__(self, model=None, in_dim, hid_dim, out_dim, activation=dy.tanh):
         ##
-        #self._W1 = model.add_parameters((hid_dim, in_dim))
-        #self._b1 = model.add_parameters(hid_dim)
-        #self._W2 = model.add_parameters((out_dim, hid_dim))
-        #self._b2 = model.add_parameters(out_dim)
+        self._W1 = model.add_parameters((hid_dim, in_dim))
+        self._b1 = model.add_parameters(hid_dim)
+        self._W2 = model.add_parameters((out_dim, hid_dim))
+        self._b2 = model.add_parameters(out_dim)
 
         #WORKS
-        self.model = dy.ParameterCollection() # this works
-        self._W1 = self.model.add_parameters((hid_dim, in_dim))
-        self._b1 = self.model.add_parameters(hid_dim)
-        self._W2 = self.model.add_parameters((out_dim, hid_dim))
-        self._b2 = self.model.add_parameters(out_dim)
+        #self.model = dy.ParameterCollection() # this works
+        #self._W1 = self.model.add_parameters((hid_dim, in_dim))
+        #self._b1 = self.model.add_parameters(hid_dim)
+        #self._W2 = self.model.add_parameters((out_dim, hid_dim))
+        #self._b2 = self.model.add_parameters(out_dim)
 
         self.activation = activation
 
